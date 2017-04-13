d874f05d38db40075bd889daeacc14b5f2dcc986
Alex I. Ramirez <alex@arcompware.com>
2017-04-13 10:20:00 -0700

    Default Output

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.get('/:system', function (req, res) {
    var fs = require('fs');
     var obj = JSON.parse(fs.readFileSync('systems.json', 'utf8'));
     var system = req.params.system;
-    res.send("<html><head><title>Derp</title></head><body>System: " + system + "<br/>System URL: " + obj[system].url + "</body></html>");
+    res.send("System: " + system + "<br/>System URL: " + obj[system].url + "<br/>System Provider: " + ob[system].type);
 })
 
 var server = app.listen(process.env.PORT || 8080, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("API listening at http://%s:%s", host, port)
 })
