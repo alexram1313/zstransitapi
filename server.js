95bfc0b78c95af155bac6f4cdc617112bd594474
Alex I. Ramirez <alex@arcompware.com>
2017-04-13 09:52:27 -0700

    Systems Test

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
+app.get('/:system', function (req, res) {
+   var fs = require('fs');
+    var obj = JSON.parse(fs.readFileSync('systems.json', 'utf8'));
+    var system = req.params.system
+    res.send(obj.system.url);
+})
+
 var server = app.listen(process.env.PORT || 8080, function () {
    var host = server.address().address
    var port = server.address().port
