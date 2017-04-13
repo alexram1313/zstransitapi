a0829268b7dcc3dad04adbaf7fa385aa75ec5367
Alex I. Ramirez <alex@arcompware.com>
2017-04-13 10:22:02 -0700

    JSON output

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/:system', function (req, res) {
    var fs = require('fs');
     var obj = JSON.parse(fs.readFileSync('systems.json', 'utf8'));
     var system = req.params.system;
-    res.send("System: " + system + "<br/>System URL: " + obj[system].url + "<br/>System Provider: " + obj[system].type);
+    
+    res.send(JSON.stringify(obj[system]));
 })
 
 var server = app.listen(process.env.PORT || 8080, function () {
