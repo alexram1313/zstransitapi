cded63c0492a97da6d3e8dd0db3749881d1e1c3b
Alex I. Ramirez <alex@arcompware.com>
2017-04-13 13:01:25 -0700

    Routing Change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 app.get('/:sys', function (req, res) {
     const sys = require('./' + getSystemType(req.params.sys));
     reg['url'] = sys.url;
-    app.use('/:sys', sys.type);
+    app.use('/', sys.type);
     // var fs = require('fs');
     // var systems = JSON.parse(fs.readFileSync('systems.json', 'utf8'));
     // var sys = req.params.sys;
