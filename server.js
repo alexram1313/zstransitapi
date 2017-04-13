123b3c125fa518e33a0e89a92dacb147f5439049
Alex I. Ramirez <alex@arcompware.com>
2017-04-13 12:15:40 -0700

    Function test

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+function getSystem(sys){
+    var fs = require('fs');
+    var systems = JSON.parse(fs.readFileSync('systems.json', 'utf8'));
+    return systems[sys].type;
+}
+
+
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
@@ -11,6 +18,13 @@ app.get('/:sys', function (req, res) {
     var sys = req.params.sys;
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify(systems[sys]));
+})
+
+app.get('/:sys/routes', function (req, res) {
+    var sys = req.params.sys;
+    var type = getSystem(sys);
+    // res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(type);
 })
 
 var server = app.listen(process.env.PORT || 8080, function () {
